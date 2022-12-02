f9ce5bc3420feb856373d38c51a423adddaa3565
shuiruohanyu <shuiruohanyu@foxmail.com>
2022-12-02 13:37:41 +0800

    调用资料action存储Vuex

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,7 +1,8 @@
 import { getToken, setToken, removeToken } from '@/utils/auth'
-import { login } from '@/api/user'
+import { login, getUserInfo } from '@/api/user'
 const state = {
-  token: getToken() // 从缓存中读取初始值
+  token: getToken(), // 从缓存中读取初始值
+  userInfo: {} // 存储用户基本资料状态
 }
 
 const mutations = {
@@ -14,6 +15,9 @@ const mutations = {
     // 删除Vuex的token
     state.token = null
     removeToken()
+  },
+  setUserInfo(state, userInfo) {
+    state.userInfo = userInfo
   }
 }
 
@@ -25,6 +29,11 @@ const actions = {
     const token = await login(data)
     // 返回一个token 123456
     context.commit('setToken', token)
+  },
+  // 获取用户的基本资料
+  async getUserInfo(context) {
+    const result = await getUserInfo()
+    context.commit('setUserInfo', result)
   }
 }
 
