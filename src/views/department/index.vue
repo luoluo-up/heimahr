7425d5c7a74cf5ad813ec2598e964f42810309a3
shuiruohanyu <shuiruohanyu@foxmail.com>
2022-12-14 12:27:58 +0800

    自定义结构获取作用域插槽数据

--- src/views/department/index.vue
+++ src/views/department/index.vue
@@ -2,7 +2,31 @@
   <div class="container">
     <div class="app-container">
       <!-- 展示树形结构 -->
-      <el-tree default-expand-all :data="depts" :props="defaultProps" />
+      <el-tree default-expand-all :data="depts" :props="defaultProps">
+        <!-- 节点结构 -->
+        <!-- v-slot="{ node, data }" 只能作用在template -->
+        <template v-slot="{ data }">
+          <el-row style="width:100%;height:40px" type="flex" justify="space-between" align="middle">
+            <el-col>{{ data.name }}</el-col>
+            <el-col :span="4">
+              <span class="tree-manager">{{ data.managerName }}</span>
+              <el-dropdown>
+                <!-- 显示区域内容 -->
+                <span class="el-dropdown-link">
+                  操作<i class="el-icon-arrow-down el-icon--right" />
+                </span>
+                <!-- 下拉菜单选项 -->
+                <el-dropdown-menu slot="dropdown">
+                  <el-dropdown-item>添加子部门</el-dropdown-item>
+                  <el-dropdown-item>编辑部门</el-dropdown-item>
+                  <el-dropdown-item>删除</el-dropdown-item>
+                </el-dropdown-menu>
+              </el-dropdown>
+            </el-col>
+          </el-row>
+        </template>
+
+      </el-tree>
     </div>
   </div>
 </template>
@@ -11,12 +35,17 @@ export default {
   name: 'Department',
   data() {
     return {
-      depts: [{ name: '传智教育', children: [{
-        name: '总裁办'
+      depts: [{ name: '传智教育', managerName: '管理员', children: [{
+        name: '总裁办',
+        managerName: '张三'
       }, {
-        name: '行政部'
+        name: '行政部',
+        managerName: '李四'
+
       }, {
-        name: '人事部'
+        name: '人事部',
+        managerName: '王五'
+
       }] }], // 数据属性
       defaultProps: {
         label: 'name', // 要显示的字段的名字
@@ -30,5 +59,10 @@ export default {
 .app-container {
   padding: 30px 140px;
   font-size: 14px;
+}
+.tree-manager {
+  width: 50px;
+  display: inline-block;
+  margin: 10px;
 }
 </style>
