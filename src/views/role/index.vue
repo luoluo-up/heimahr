cf9d09153752095f1a341597f1068435b02b5f0b
shuiruohanyu <shuiruohanyu@foxmail.com>
2022-12-22 20:54:41 +0800

    角色管理弹出添加层

--- src/views/role/index.vue
+++ src/views/role/index.vue
@@ -3,7 +3,7 @@
     <div class="app-container">
       <!-- 角色管理内容 -->
       <div class="role-operate">
-        <el-button size="mini" type="primary">添加角色</el-button>
+        <el-button size="mini" type="primary" @click="showDialog = true">添加角色</el-button>
       </div>
       <!-- 放置table组件 -->
       <el-table :data="list">
@@ -37,6 +37,29 @@
         />
       </el-row>
     </div>
+    <!-- 放置弹层 -->
+    <el-dialog width="500px" title="新增角色" :visible.sync="showDialog">
+      <!-- 表单内容 -->
+      <el-form label-width="120px">
+        <el-form-item label="角色名称">
+          <el-input style="width:300px" size="mini" />
+        </el-form-item>
+        <el-form-item label="启用">
+          <el-switch size="mini" />
+        </el-form-item>
+        <el-form-item label="角色描述">
+          <el-input type="textarea" :rows="3" style="width:300px" size="mini" />
+        </el-form-item>
+        <el-form-item>
+          <el-row type="flex" justify="center">
+            <el-col :span="12">
+              <el-button type="primary" size="mini">确定</el-button>
+              <el-button size="mini">取消</el-button>
+            </el-col>
+          </el-row>
+        </el-form-item>
+      </el-form>
+    </el-dialog>
   </div>
 </template>
 <script>
@@ -46,6 +69,7 @@ export default {
   data() {
     return {
       list: [],
+      showDialog: false, // 控制弹层显示隐藏
       // 将分页信息放置到一个对象中
       pageParams: {
         page: 1, // 第几页
