17ff3196c3da8ccf88d89c727c8febb8a38ff37f
shuiruohanyu <shuiruohanyu@foxmail.com>
2022-12-28 00:29:06 +0800

    新增角色确定和取消

--- src/views/role/index.vue
+++ src/views/role/index.vue
@@ -38,13 +38,14 @@
       </el-row>
     </div>
     <!-- 放置弹层 -->
-    <el-dialog width="500px" title="新增角色" :visible.sync="showDialog">
+    <el-dialog width="500px" title="新增角色" :visible.sync="showDialog" @close="btnCancel">
       <!-- 表单内容 -->
       <el-form ref="roleForm" :model="roleForm" :rules="rules" label-width="120px">
         <el-form-item prop="name" label="角色名称">
           <el-input v-model="roleForm.name" style="width:300px" size="mini" />
         </el-form-item>
-        <el-form-item label="启用">
+        <el-form-item label="启用" prop="state">
+          <!-- 重置表单数据 需要prop属性 -->
           <!-- 如果不需要校验 就不需要写 prop属性 -->
           <el-switch v-model="roleForm.state" :active-value="1" :inactive-value="0" size="mini" />
         </el-form-item>
@@ -54,8 +55,8 @@
         <el-form-item>
           <el-row type="flex" justify="center">
             <el-col :span="12">
-              <el-button type="primary" size="mini">确定</el-button>
-              <el-button size="mini">取消</el-button>
+              <el-button type="primary" size="mini" @click="btnOK">确定</el-button>
+              <el-button size="mini" @click="btnCancel">取消</el-button>
             </el-col>
           </el-row>
         </el-form-item>
@@ -64,7 +65,7 @@
   </div>
 </template>
 <script>
-import { getRoleList } from '@/api/role'
+import { getRoleList, addRole } from '@/api/role'
 export default {
   name: 'Role',
   data() {
@@ -102,6 +103,20 @@ export default {
     changePage(newPage) {
       this.pageParams.page = newPage // 赋值当前页码
       this.getRoleList()
+    },
+    btnOK() {
+      this.$refs.roleForm.validate(async isOK => {
+        if (isOK) {
+          await addRole(this.roleForm)
+          this.$message.success('新增角色成功')
+          this.getRoleList()
+          this.btnCancel()
+        }
+      })
+    },
+    btnCancel() {
+      this.$refs.roleForm.resetFields() // 重置表单数据
+      this.showDialog = false // 关闭弹层
     }
   }
 }
