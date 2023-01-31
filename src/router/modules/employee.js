14ab49f2fefcdd220fff386f90f162b4badc640b
shuiruohanyu <shuiruohanyu@foxmail.com>
2023-01-31 14:19:45 +0800

    员工详情的结构和路由配置

--- src/router/modules/employee.js
+++ src/router/modules/employee.js
@@ -10,5 +10,14 @@ export default {
       title: '员工',
       icon: 'people'
     }
+  }, {
+    path: '/employee/detail', // 员工详情的地址
+    component: () => import('@/views/employee/detail.vue'),
+    hidden: true, // 表示隐藏在左侧菜单
+    meta: {
+      title: '员工详情' // 显示在导航的文本
+    }
   }]
 }
+
+// 只想在左侧菜单显示一级菜单的话 让二级路由只有一个显示在左侧菜单
