fae93d05845417442f0c4120150beab53c4ab885
shuiruohanyu <shuiruohanyu@foxmail.com>
2023-02-09 15:54:23 +0800

    角色分配权限的确定和取消

--- src/api/role.js
+++ src/api/role.js
@@ -54,3 +54,16 @@ export function getRoleDetail(id) {
     url: `/sys/role/${id}`
   })
 }
+
+/**
+ * 给角色分配权限
+ *
+ * ***/
+
+export function assignPerm(data) {
+  return request({
+    url: '/sys/role/assignPrem',
+    method: 'put',
+    data
+  })
+}
