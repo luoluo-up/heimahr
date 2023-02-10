1a7a58cd1df44cca21f047a16de62129282e445a
shuiruohanyu <shuiruohanyu@foxmail.com>
2023-02-10 14:20:36 +0800

    拆分静态路由和动态路由

--- src/router/index.js
+++ src/router/index.js
@@ -62,6 +62,12 @@ export const constantRoutes = [
       meta: { title: '首页', icon: 'dashboard' }
     }]
   },
+
+  // 404 page must be placed at the end !!!
+  { path: '*', redirect: '/404', hidden: true }
+]
+// 动态路由
+export const asyncRoutes = [
   departmentRouter,
   roleRouter,
   employeeRouter,
@@ -69,15 +75,11 @@ export const constantRoutes = [
   attendanceRouter,
   approvalRouter,
   salaryRouter,
-  socialRouter,
-  // 404 page must be placed at the end !!!
-  { path: '*', redirect: '/404', hidden: true }
-]
-
+  socialRouter]
 const createRouter = () => new Router({
   // mode: 'history', // require service support
   scrollBehavior: () => ({ y: 0 }),
-  routes: constantRoutes
+  routes: constantRoutes // 默认引入静态路由
 })
 
 const router = createRouter()
