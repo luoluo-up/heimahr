f9459dde8ee47f500c664ad3eec8f6a125255727
shuiruohanyu <shuiruohanyu@foxmail.com>
2023-02-10 19:21:57 +0800

    根据权限渲染左侧菜单

--- src/layout/components/Sidebar/index.vue
+++ src/layout/components/Sidebar/index.vue
@@ -30,13 +30,13 @@ export default {
   components: { SidebarItem, Logo },
   computed: {
     ...mapGetters([
-      'sidebar'
+      'sidebar', 'routes'
     ]),
     // 路由信息的计算属性
-    routes() {
-      // 返回所有的路由信息
-      return this.$router.options.routes
-    },
+    // routes() {
+    //   // 返回所有的路由信息
+    //   return this.$router.options.routes
+    // },
     activeMenu() {
       const route = this.$route
       const { meta, path } = route
--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,8 +1,10 @@
 import { getToken, setToken, removeToken } from '@/utils/auth'
 import { login, getUserInfo } from '@/api/user'
+import { constantRoutes } from '@/router'
 const state = {
   token: getToken(), // 从缓存中读取初始值
-  userInfo: {} // 存储用户基本资料状态
+  userInfo: {}, // 存储用户基本资料状态
+  routes: constantRoutes // 静态路由的数组
 }
 
 const mutations = {
@@ -18,6 +20,9 @@ const mutations = {
   },
   setUserInfo(state, userInfo) {
     state.userInfo = userInfo
+  },
+  setRoutes(state, newRoutes) {
+    state.routes = [...constantRoutes, ...newRoutes] // 静态路由 + 动态路由
   }
 }
 
