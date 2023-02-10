03b7e3eb3b3d2c428724769b6b926dbbcad37e74
shuiruohanyu <shuiruohanyu@foxmail.com>
2023-02-10 17:51:39 +0800

    添加动态路由信息

--- src/permission.js
+++ src/permission.js
@@ -2,6 +2,7 @@ import router from '@/router'
 import nprogress from 'nprogress'
 import 'nprogress/nprogress.css'
 import store from '@/store'
+import { asyncRoutes } from '@/router'
 
 /**
  *前置守卫
@@ -21,9 +22,19 @@ router.beforeEach(async(to, from, next) => {
     } else {
       // 判断是否获取过资料
       if (!store.getters.userId) {
-        await store.dispatch('user/getUserInfo')
+        const { roles } = await store.dispatch('user/getUserInfo')
+        // console.log(roles.menus) // 数组 不确定 可能是8个 1个 0个
+        // console.log(asyncRoutes) // 数组 8个
+        const filterRoutes = asyncRoutes.filter(item => {
+          // return true/false
+          return roles.menus.includes(item.name)
+        }) // 筛选后的路由
+        router.addRoutes([...filterRoutes, { path: '*', redirect: '/404', hidden: true }]) // 添加动态路由信息到路由表
+        // router添加动态路由之后 需要转发一下
+        next(to.path) // 目的是让路由拥有信息 router的已知缺陷
+      } else {
+        next() // 放过
       }
-      next() // 放过
     }
   } else {
     // 没有token
--- src/router/index.js
+++ src/router/index.js
@@ -61,10 +61,10 @@ export const constantRoutes = [
       component: () => import('@/views/dashboard/index'),
       meta: { title: '首页', icon: 'dashboard' }
     }]
-  },
+  }
 
   // 404 page must be placed at the end !!!
-  { path: '*', redirect: '/404', hidden: true }
+
 ]
 // 动态路由
 export const asyncRoutes = [
