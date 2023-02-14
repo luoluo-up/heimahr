694ad50a9f4d84737c84f8f4a32e8a602ab51da3
shuiruohanyu <shuiruohanyu@foxmail.com>
2023-02-14 15:30:54 +0800

    权限应用-功能权限管理

--- src/main.js
+++ src/main.js
@@ -35,6 +35,23 @@ Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
+// 封装自定义指令 用来控制操作权
+Vue.directive('permission', {
+  // 会在指令作用的元素插入到页面完成以后触发
+  inserted(el, binding) {
+    // el 指令作用的元素的dom对象
+    console.log(el)
+    const points = store.state.user.userInfo?.roles?.points || [] // 当前用户信息的操作权
+    if (!points.includes(binding.value)) {
+      // 不存在就要删除或者禁用
+      el.remove() // 删除元素
+      // el.disabled = true
+      // 线上的权限数据和线下的代码进行对应
+    }
+  }
+
+})
+
 new Vue({
   el: '#app',
   router,
